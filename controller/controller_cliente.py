b2f1dc6f7963d81d8ded2ae78a211a8593fef4fe
rafa-python <rafadeveloper10@gmail.com>
2023-05-19 15:44:53 -0300

    criei a pasta controller para modularizar minha aplicação

---
+++ controller/controller_cliente.py
@@ -0,0 +1,53 @@
+from prettytable import PrettyTable
+from dao import DaoCliente
+
+
+class ControllerCliente:
+    @classmethod
+    def add(cls, nome, fone, cpf):
+        resposta = ""
+        if DaoCliente.add("Cliente", nome=nome, fone=fone, cpf=cpf):
+            resposta = "Cliente adicionado com sucesso"
+        else:
+            resposta = "Infelizmente ocorreu um erro no cadastro"
+
+        return resposta
+
+    @classmethod
+    def exibe_clientes(cls):
+        print("====================================================")
+        tabela = PrettyTable(["ID", "NOME", "FONE", "CPF"])
+        dados = DaoCliente.retorna_dados("Cliente")
+        [tabela.add_row(linha) for linha in dados]
+
+        return tabela
+
+    @classmethod
+    def editar_cliente(cls, id, nome, fone, cpf):
+        resposta = ""
+        if nome != "":
+            DaoCliente.edit("Cliente", id, nome=nome)
+            resposta += "Nome atualizado\n"
+
+        if fone != "":
+            DaoCliente.edit("Cliente", id, fone=fone)
+            resposta += "Fone atualizado\n"
+
+        if cpf != "":
+            DaoCliente.edit("Cliente", id, cpf=cpf)
+            resposta += "CPF atualizado"
+
+        if cpf == "" and fone == "" and nome == "":
+            resposta = "Nada para atualizar"
+
+        return resposta
+
+    @classmethod
+    def excluir_cliente(cls, id):
+        DaoCliente.excluir(id, "id", "Cliente")
+
+        return "Cliente excluido com sucesso"
+
+    @classmethod
+    def verifica_se_existe_cliente(cls, dado, campo):
+        return DaoCliente.verifica_se_existe(dado, campo, "Cliente")
---
+++ controller/controller_vendas.py
@@ -0,0 +1,13 @@
+from dao import DaoVenda
+from prettytable import PrettyTable
+
+
+class Vendas:
+    @classmethod
+    def exibe(cls):
+        pass
+
+
+
+if __name__ == "__main__":
+    pass
